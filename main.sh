6e567baffbc42532eb9e61984d516e64eec692f7
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:28:27 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,18 +1,11 @@
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
-
 # Clone Upstream
 git clone https://github.com/linuxmint/mintsystem
 cp -rvf ./debian ./mintsystem
 cd ./mintsystem
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 apt-get build-dep ./ -y
 
 # Build package
